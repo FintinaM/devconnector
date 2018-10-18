ecb223d745b965f426e12ec7fae6719e5e9e6f94
Fintina <fintina@codebeacon.net>
2018-10-18 21:30:07 +0300

    Commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://FintinaM:Dinozaur1@ds133533.mlab.com:33533/devconnector'
+    mongoURI: 'mongodb://FintinaM:Dinozaur1@ds133533.mlab.com:33533/devconnector',
+    secretOrKey: 'secret'
 };
